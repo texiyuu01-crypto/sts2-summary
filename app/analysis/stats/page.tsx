f6360dc8bb625f49d1d9dd5d7247462064928f10
texiyuu01-crypto <texiyuu01@gmail.com>
2026-04-23 10:42:54 +0900

    Fix useState error by moving UpdatedAtDisplay to separate client component

--- app/analysis/stats/page.tsx
+++ app/analysis/stats/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StatsGridClient from './StatsGridClient';
+import UpdatedAtDisplay from './UpdatedAtDisplay';
 
 export const dynamic = 'force-dynamic';
 export const runtime = 'edge';
@@ -28,17 +29,4 @@ export default function TierPage() {
       </div>
     </main>
   );
-}
-
-function UpdatedAtDisplay() {
-  const [updatedAt, setUpdatedAt] = React.useState<string>('Loading...');
-
-  React.useEffect(() => {
-    fetch('/data/updated_at.json')
-      .then(res => res.json())
-      .then(data => setUpdatedAt(data.updated_at || 'N/A'))
-      .catch(() => setUpdatedAt('N/A'));
-  }, []);
-
-  return <span>{updatedAt}</span>;
 }
